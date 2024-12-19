b7606b8834563362e929dd370c95b2063cfee847
Nicolas Vazquez <62533042+nicolasvazquez95@users.noreply.github.com>
2024-12-19 17:32:43 -0300

    Update deployment configuration for documentation to use GitHub Pages

--- docs/make.jl
+++ docs/make.jl
@@ -21,6 +21,8 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(;
+    repo = "github.com/nicolasvazquez95/YardSale.jl.git",
+    branch = "gh-pages",
+    target = "build"
+)
